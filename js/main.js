1c7c99067bbb043a8089d7220a15b9f582fea12b
aphilip97 <33058483+aphilip97@users.noreply.github.com>
2018-06-19 23:20:41 +0100

    Game Of Life Added

--- js/main.js
+++ js/main.js
@@ -2,19 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var contentSegments = document.querySelectorAll('.content-segment');
 
-    var colours = [
-        "#616161",
-        "#9C27B0",
-        "#F44336",
-        "#1565C0"
-    ];
+    var colours = ["#111111"];
 
-    var images = [
-        "./images/logo.svg",
-        "./images/logo.svg",
-        "./images/logo.svg",
-        "./images/logo.svg"
-    ];
+    var images = ["./images/gameOfLife.PNG"];
 
     var colourCounter = 0;
     for(let i = 0; i < contentSegments.length; i++, colourCounter++) {
